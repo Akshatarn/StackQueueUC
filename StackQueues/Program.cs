58fe04830cab3b1d717d1b82f9cd7ae9ae31e684
akshata_rn <akshatarn123@gamail.com>
2022-12-05 22:49:56 +0530

    Peek and Pop Use case

--- StackQueues/Program.cs
+++ StackQueues/Program.cs
@@ -12,7 +12,8 @@ namespace StackQueues
         {
             Console.WriteLine("Welcome to Stack Queue Program");
             Console.WriteLine("Please select as per the below given options :\n" +
-                "Press 1: Creating custom stack using LinkedList.\n ");
+                "Press 1: Creating custom stack using LinkedList.\n "+
+                "Press 2: Using Peek and Pop in stack.\n");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -22,6 +23,15 @@ namespace StackQueues
                     stackObject.Push(30);
                     stackObject.Push(56);
                     stackObject.Display();
+                    break;
+                case 2:
+                    LinkedListStackQueue stackObject1 = new LinkedListStackQueue();
+                    stackObject1.Push(70);
+                    stackObject1.Push(30);
+                    stackObject1.Push(56);
+                    stackObject1.Display();
+                    Console.WriteLine("Is list is empty? " + stackObject1.isEmpty());
+                    stackObject1.Display();
                     break;
                 default:
                     break;
