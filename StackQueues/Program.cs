0762830373c2b0797a207be044eafc47cc5098be
akshata_rn <akshatarn123@gamail.com>
2022-12-05 22:41:54 +0530

    Stack is created

--- StackQueues/Program.cs
+++ StackQueues/Program.cs
@@ -11,6 +11,21 @@ namespace StackQueues
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack Queue Program");
+            Console.WriteLine("Please select as per the below given options :\n" +
+                "Press 1: Creating custom stack using LinkedList.\n ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    LinkedListStackQueue stackObject = new LinkedListStackQueue();
+                    stackObject.Push(70);
+                    stackObject.Push(30);
+                    stackObject.Push(56);
+                    stackObject.Display();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
